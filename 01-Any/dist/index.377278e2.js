687b22341e6013681745ca2a90b97ff7a6c319f6
GazKotlin <gaz.java@hotmail.com>
2024-05-31 14:38:51 -0600

    01-any

---
+++ 01-Any/dist/index.377278e2.js
@@ -0,0 +1,16 @@
+let mensaje = "01-ANY";
+console.log(mensaje);
+let firstName = "John Doe";
+console.log(firstName);
+firstName = 123;
+console.log(firstName);
+firstName = [
+    1,
+    2,
+    3
+];
+console.log(firstName); // function mul (number){
+ //     return number *2;
+ // }
+
+//# sourceMappingURL=index.377278e2.js.map
---
+++ 01-Any/src/index.ts
@@ -0,0 +1,13 @@
+let mensaje = "01-ANY";
+console.log(mensaje);
+
+let firstName:any = "John Doe";
+console.log(firstName);
+firstName = 123;
+console.log(firstName);
+firstName = [1, 2, 3];
+console.log(firstName);
+
+// function mul (number){
+//     return number *2;
+// }
